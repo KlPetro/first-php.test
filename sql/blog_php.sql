4faca6b0d871bc4e792ac25864420f9d2a74517f
KlPetro <107338572+KlPetro@users.noreply.github.com>
2022-11-06 10:12:30 +0200

    06.11

--- sql/blog_php.sql
+++ sql/blog_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Час створення: Лис 04 2022 р., 09:40
+-- Час створення: Лис 06 2022 р., 09:07
 -- Версія сервера: 10.4.25-MariaDB
 -- Версія PHP: 8.1.10
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблиці `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `twit` text NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `posts`
+--
+
+INSERT INTO `posts` (`id`, `twit`, `user_id`, `created`) VALUES
+(1, 'SELECT * FROM `users`\r\nSELECT * FROM `posts`\r\nTRUNCATE TABLE `blog_php`.`posts`\r\nSELECT * FROM `posts`\r\nSELECT * FROM `posts`', '1', '2022-11-06 00:14:28'),
+(2, 'SELECT * FROM `users`\r\nSELECT * FROM `posts`\r\nTRUNCATE TABLE `blog_php`.`posts`\r\nSELECT * FROM `posts`\r\nSELECT * FROM `posts`SELECT * FROM `users`\r\nSELECT * FROM `posts`\r\nTRUNCATE TABLE `blog_php`.`posts`\r\nSELECT * FROM `posts`\r\nSELECT * FROM `posts`', '2', '2022-11-06 00:14:28');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблиці `users`
 --
 
@@ -31,21 +52,28 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `role` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп даних таблиці `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'Greg', 'greg@i.ua', '123456'),
-(2, 'qwe', 'qwer@i.ua', '12345'),
-(3, 'q', 'q@q', '123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'Greg', 'greg@i.ua', '123456', 'user'),
+(2, 'qwe', 'qwer@i.ua', '12345', 'user'),
+(3, 'q', 'q@q', '123', 'admin');
 
 --
 -- Індекси збережених таблиць
 --
+
+--
+-- Індекси таблиці `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Індекси таблиці `users`
@@ -56,6 +84,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT для збережених таблиць
 --
+
+--
+-- AUTO_INCREMENT для таблиці `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблиці `users`
