22cdb0f4a96d0cc0aca2ca3ad112058484825152
KlPetro <107338572+KlPetro@users.noreply.github.com>
2022-11-09 21:27:04 +0200

    baza

--- sql/blog_php.sql
+++ sql/blog_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Час створення: Лис 07 2022 р., 08:37
+-- Час створення: Лис 09 2022 р., 12:03
 -- Версія сервера: 10.4.25-MariaDB
 -- Версія PHP: 8.1.10
 
@@ -31,19 +31,25 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `twit` text NOT NULL,
   `user_id` varchar(255) NOT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp()
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп даних таблиці `posts`
 --
 
-INSERT INTO `posts` (`id`, `twit`, `user_id`, `created`) VALUES
-(1, 'Disabled buttons using <a> should include the aria-disabled=\"true\" attribute to indicate the state of the element to assistive technologies.', '1', '2022-11-06 00:14:28'),
-(2, 'Оператори порівняння виконують порівнювання заданих значень та повертають True (істина) або False (брехня).++', '9', '2022-11-06 00:14:28'),
-(3, '123234345rtgdfgdfg', '3', '2022-11-06 21:16:38'),
-(7, 'aaaaa', '10', '2022-11-07 00:03:16'),
-(12, 'kokoko', '3', '2022-11-07 09:09:46');
+INSERT INTO `posts` (`id`, `twit`, `user_id`, `created`, `image`) VALUES
+(1, ' ons using <a> should include the aria-disabled=\"true\" attribute to indicate the state of the element to assistive technologies.', '1', '2022-11-06 00:14:28', ''),
+(2, 'Оператори порівняння виконують порівнювання заданих значень та повертають True (істина) або False (брехня).++', '9', '2022-11-06 00:14:28', ''),
+(7, 'Оператори порівняння виконують порівнювання заданих значень та повертають True (істина) або False (брехня).++', '10', '2022-11-07 00:03:16', ''),
+(12, 'Телеканал «ТЕТ-А-ТЕТ» (пізніше — «ТЕТ») був заснований 1998 року як один з перших приватних телеканалів в Україні. Почав сдва канали відтоді мають змогу вести цілодобове мовлення.  2001 року телеканал отримав статус регіонального, а 2003 року — загальнонаціонального.  24 квітня 2004 року «ТЕТ» змінив логотип та графічне мовлення. З того часу в етері представлені реаліті-шоу, ток-шоу, телесеріали, гумористичні програми, мультфільми, спортивні трансляції. На телеканалі відсутні політичні програми, новини. Покриття каналу охоплює 92,4 % території України[6].  З 2009 року «ТЕТ» входить до складу медіаконгломерату «1+1 Media».  У листопаді 2010 року генеральною продюсеркою телеканалу стала Ірина Костюк.', '3', '2022-11-07 09:09:46', ''),
+(38, 'new photo', '3', '2022-11-08 22:06:44', ''),
+(39, 'new photo2', '3', '2022-11-08 22:08:14', '8oQHC5RjxbPWrWTXFiM0k2kS2JeRI6kS6UdEypqVZY5gVAOUzMRfMWlzUgIHDvpp1667938094.png'),
+(40, '234wer', '3', '2022-11-08 22:21:03', 's6Be58kCOeB9uSAXgw1uACrQ6UULKimQzSnE1olJnd1RXqx4EuE6whttWvvSq7AX1667938863.png'),
+(42, 'Github', '1', '2022-11-09 11:11:29', 'QG0B0f8anBphAOO6pBQau9dKpJqbLJdis9FvkpQIgcWzjswefpTDdVC5PBC83cQH1667985089.png'),
+(43, 'qwe', '3', '2022-11-09 11:27:23', '8hGoxO2hmzHLdlrbpsekDR8Z1p4QZyZ4jYMzkXNtzMEaeHrGFK6i6A2KqZMoz3li1667986043.png'),
+(44, 'instagram', '13', '2022-11-09 11:40:03', 'QSqWYij5tgD7duKCUfI1z6VYqZXBuOHtlRsOJ7X2ISuPUwG4ALlWeN5gNAku82ha1667986803.png');
 
 -- --------------------------------------------------------
 
@@ -64,11 +70,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
-(1, 'Greg', 'greg@i.ua', '123456', 'user'),
-(3, 'q', 'q@q', '123', 'admin'),
-(9, 'west', 'west@q', NULL, 'user'),
-(10, 'west22', 'west22@q', NULL, 'user'),
-(11, 'Greg', '11@11', NULL, 'user');
+(1, 'Greg', 'greg@i.ua', '123456', 'admin'),
+(3, 'Petro', 'q@q', '123', 'admin'),
+(13, 'west22', 'we@w', '123', 'user'),
+(14, '123', 'we@w', '123', 'user');
 
 --
 -- Індекси збережених таблиць
@@ -94,13 +99,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблиці `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT для таблиці `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
